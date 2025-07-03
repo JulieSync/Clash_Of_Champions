ece593e15754b3148cbd2435916ed41f2cdcef45
LemonSync <grouplemon0@gmail.com>
2025-07-03 11:34:23 +0700

    Update All

--- api/generate-image.js
+++ api/generate-image.js
@@ -43,7 +43,7 @@ function isGloballyRateLimited() {
   return false;
 }
 
-Canvas.registerFont(path.join(__dirname, '..\media\fonts\Magistral Regular.otf'), { family: 'default' });
+Canvas.registerFont(path.join(__dirname, '..\media\fonts\moby-reg.ttf'), { family: 'default' });
 
 function wrapText(ctx, text, centerX, startY, maxWidth, lineHeight) {
   const words = text.split(' ');
